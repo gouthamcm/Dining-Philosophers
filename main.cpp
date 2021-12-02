2256e142105aed9203e269041e15fca639015290
Goutham C M <goutham.898@gmail.com>
2021-12-02 16:16:41 +0530

    Added functions for the philosopher

--- main.cpp
+++ main.cpp
@@ -1,6 +1,54 @@
+/* Created by Goutham C M
+   on Dec 2 2021 */
+
 #include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+#define PHILOSOPHERS_COUNT 5
+
 using namespace std;
 
+
+sem_t number_of_rooms;
+sem_t chopsticks[PHILOSOPHERS_COUNT];
+
+void thinking(int philosopher){
+    cout<<"Philosopher "<<philosopher<<" is thinking\n";
+    return ;
+}
+
+void eating_init(int philosopher){
+    cout<<"Philosopher "<<philosopher<<" is going to eating\n";
+    return ;
+}
+
+void eating_done(int philosopher){
+    cout<<"Philosopher "<<philosopher<<" is done eating\n";
+    return ;
+}
+
+void * dining_philosophers(void *arg){
+
+}
+
 int main(int argc, char* argv[]){
+    int number_of_philosophers;
+    cout<<"Hi! Welcome to the puzzle\nEnjoy the conference\n";
+    
+    sem_init(&number_of_rooms, 0, 4);
+    for(int i=0; i<PHILOSOPHERS_COUNT; i++){
+        sem_init(&chopsticks[i], 0, 1);
+    }
+
+    pthread_t thread[PHILOSOPHERS_COUNT];
+
+    for(int i=0; i<PHILOSOPHERS_COUNT; i++){
+        pthread_create(&thread[i], NULL, dining_philosophers, (void *)&i);
+    }
+    for(int i=0; i<PHILOSOPHERS_COUNT; i++){
+        pthread_join(thread[i], NULL);
+    }
     return 0;
 }
